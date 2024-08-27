00def39e6a05fe237c6aae053e1482dd9f0db01e
Himanshipatel04 <himanshipatel0409@gmail.com>
2024-08-27 22:24:49 +0530

    d

--- yt_notes/yt_notes/settings.py
+++ yt_notes/yt_notes/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u0#56fj95y(1yo)vx0!nbxgzf@zu2+d-85(#=w9awr!nbqai8)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
